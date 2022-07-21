443b0574a43b3311a54bffeb0bb14dfe9be8d139
jimblizzard <jimblizz@microsoft.com>
2022-07-21 17:45:39 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/event/{id}", async (http) => 
+    { await Task.Run(() => 
+        {
+            var id = http.Request.RouteValues["id"];
+            http.Response.StatusCode = 200;
+            http.Response.ContentType = "text/plain";
+            http.Response.WriteAsync($"Event {id} has been received");
+        }); 
+    });
+
+app.Run();
